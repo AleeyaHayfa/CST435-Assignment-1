e5d3dec4ecf53fabcaf3bc2713eddbbc132c0006
Jasmine Chung <jasminechung02@gmail.com>
2025-11-27 14:18:32 +0800

    Remove unwanted folders/files from repo

--- common/client_config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-def get_target(service_name, default_port):
-    if os.getenv("DOCKER_MODE") == "1":
-        return f"http://{service_name}:{default_port}"
-    else:
-        return f"http://localhost:{default_port}"
